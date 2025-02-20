219ad1d4dc357584dbd19b87241e690e6cb827d9
Vusall-Elshenoghlu <vusaleh-af206@code.edu.az>
2025-02-20 13:18:47 +0400

    02-20-2025 13:38

--- frontend/src/pages/User/DentalCareDetail/ChildrensTeeth.jsx
+++ frontend/src/pages/User/DentalCareDetail/ChildrensTeeth.jsx
@@ -6,69 +6,51 @@ function ChildrensTeeth() {
   const { lang, } = useContext(LanguageContext);
   const translations = {
     az: {
-      care: "Diş qayğısı",
-      misalignedTeeth: "Düzgün olmayan dişlər diş ətinin iltihabına, diş çürüməsinə və nitq pozuntularına səbəb ola bilər.",
-      createSmile: "Mükəmməl təbəssüm yaradın",
-      orthodonticsEffect: "Ortodontiya dişlərin düzgün düzülməsi ilə mükəmməl təbəssüm yarada bilər və bu, şəxsin özünə inamına çox müsbət təsir göstərə bilər.",
-      alignmentDescription: "Dişlərin düzülməsi adətən müəyyən bir müddət ərzində taxılan sabit və ya çıxarılan cihazlar vasitəsilə həyata keçirilir. Bu cihazlar, düzgün saxlanmadıqda, diş problemlərinə səbəb ola bilər.",
-      toothBrushing: "Diş fırçalama",
-      toothBrushingDesc: "Diş fırçasını braketlərin altına qoyun və titrəyən hərəkətlə fırçalayın; sonra fırçanı braketlərin üstünə qoyub aşağı istiqamətə fırçalayın.",
-      interdentalBrush: "İnterdental fırça",
-      interdentalBrushDesc: "Bu növ fırçanın kiçik başı braketlər arasına çatmaq üçün idealdır.",
-      flossThreader: "Diş sapı keçirmə",
-      flossThreaderDesc: "Dişlər arasında diş sapı istifadə etmək vacibdir və bu yalnız sap keçirmə vasitəsi ilə əldə edilə bilər.",
-      oralIrrigation: "Ağız suvarma",
-      oralIrrigationDesc: "Bu cihazlar qida hissəciklərini çıxarmaq üçün faydalıdır, lakin yuxarıda qeyd edilən məhsullarla birgə istifadə edilməlidir.",
-      diet: "Dieta",
-      dietDesc: "Bu cihazlar qida hissəciklərini çıxarmaq üçün faydalıdır, lakin yuxarıda qeyd edilən məhsullarla birgə istifadə edilməlidir.",
-      homeCare: "Evdə qayğıya adi haldan bir az çox vaxt lazım olacaq, lakin gözəl təbəssüm üçün vaxt ayırmağa dəyər."
+      title: "Süd dişləri önəmlidir",
+      par1:"Uşaq dişlərinin düzgün qayğısına onlara ağızda çıxmağa başladığı andan etibarən başlanmalıdır. İlk olaraq, dişləri 'parlatmaq' üçün nəmlənmiş üz örtüyü istifadə edilməli, lakin daha çox diş çıxmağa başladıqca, uşağın diş fırçası ən azı gündə iki dəfə yumşaq şəkildə istifadə olunmalıdır. Düzgün dozada verilən florid, diş minəsini daha güclü edir. Bir çox icmalarda sudan florid əlavə olunsa da, bu həmçinin hər peşəkar təmizləmədən sonra dişlərə tətbiq olunur.",
+      subtitle: "Ön dişlər 6 yaşında dəyişir. Ancaq arxa süd dişləri 10-13 yaşa qədər dəyişmir.",
+      description: "Süd dişləri çeynəmə, daimi dişlər üçün yer saxlama və çənənin normal inkişafına kömək edir.",
+      thumbSucking: "Barmaq əmmə",
+      nursingBottleCavities: "Biberon kariyesi",
+      eruptionOfTeeth: "Dişlərin çıxması",
+      dentalEmergencies: "Diş təcili yardımı",
+      grinding: "Diş qıcama (Bruksizm)",
+      orthodontics: "Ortodontiya"
     },
     en: {
-      care: "Dental care",
-      misalignedTeeth: "Misaligned teeth may lead to gum disease, tooth decay, and speech impairment.",
-      createSmile: "Create the perfect smile",
-      orthodonticsEffect: "Orthodontics can create the perfect smile through proper alignment of the teeth, which can have a positive effect on self-esteem.",
-      alignmentDescription: "The alignment of teeth is usually achieved by means of fixed or removable appliances worn over a period of time. These appliances, although effective, can create dental problems if not maintained correctly.",
-      toothBrushing: "Tooth brushing",
-      toothBrushingDesc: "Place the toothbrush below the brackets and brush upwards, then above the brackets and brush downwards.",
-      interdentalBrush: "Interdental brush",
-      interdentalBrushDesc: "The small head on this brush is ideal for reaching between brackets.",
-      flossThreader: "Floss threader",
-      flossThreaderDesc: "Flossing between teeth is important and can be achieved with a floss threader.",
-      oralIrrigation: "Oral irrigation",
-      oralIrrigationDesc: "These devices are helpful for dislodging food particles but should be used alongside the above products.",
-      diet: "Diet",
-      dietDesc: "These devices are useful for removing food particles but should be used alongside the products above.",
-      homeCare: "Home care will take a bit longer than usual, but the benefits of a beautiful smile are worth the extra time."
+      title: "Primary teeth are important",
+      subtitle: "The front teeth are replaced by the age of 6 years. However, the primary posterior teeth are not replaced by their counterparts until age 10 to 13 years.",
+      par1:"Proper care of baby teeth should begin as soon as they start to appear in the mouth. At first, a moist face cloth should be used to “polish” the teeth, but as more teeth erupt, a child’s toothbrush should be used gently at least twice a day. Fluoride administered in the correct dosage makes enamel stronger. Although many communities have fluoride added to the water, it is also applied to teeth after every professional cleaning.",
+      description: "Primary teeth are important for chewing, maintaining space for permanent teeth, and aiding jaw development.",
+      thumbSucking: "Thumb sucking",
+      thumb:"This habit usually disappears by the age of 4 years. The persistence of this habit can have a detrimental effect on the teeth and the surrounding bone. Upper teeth are pushed out, lower teeth pushed in, resulting in a gap between upper and lower teeth causing difficulty in chewing, swallowing, and speech. An oral appliance can be placed in the child’s mouth to prevent thumb sucking. This device can break the habit in a few days.",
+      nursingBottleCavities: "Nursing bottle cavities",
+      eruptionOfTeeth: "Eruption of teeth",
+      dentalEmergencies: "Dental emergencies",
+      grinding: "Grinding (Bruxism)",
+      orthodontics: "Orthodontics"
     },
     ru: {
-      care: "Уход за зубами",
-      misalignedTeeth: "Неправильное расположение зубов может привести к воспалению десен, кариесу и нарушению речи.",
-      createSmile: "Создайте идеальную улыбку",
-      orthodonticsEffect: "Ортодонтия может создать идеальную улыбку за счет правильного расположения зубов, что положительно влияет на самооценку.",
-      alignmentDescription: "Выравнивание зубов обычно достигается с помощью несъемных или съемных аппаратов, которые носят в течение определенного времени. Однако при неправильном уходе они могут вызвать проблемы с зубами.",
-      toothBrushing: "Чистка зубов",
-      toothBrushingDesc: "Разместите зубную щетку под брекетами и чистите вверх, затем над брекетами и чистите вниз.",
-      interdentalBrush: "Межзубная щетка",
-      interdentalBrushDesc: "Маленькая головка этой щетки идеально подходит для очистки пространства между брекетами.",
-      flossThreader: "Зубная нить с нитьевдевателем",
-      flossThreaderDesc: "Чистка зубной нитью между зубами важна и может быть выполнена с помощью нитьевдевателя.",
-      oralIrrigation: "Орошение полости рта",
-      oralIrrigationDesc: "Эти устройства помогают удалять остатки пищи, но их следует использовать вместе с вышеуказанными средствами.",
-      diet: "Диета",
-      dietDesc: "Эти устройства помогают удалять остатки пищи, но их следует использовать в сочетании с вышеуказанными средствами.",
-      homeCare: "Уход за зубами дома займет немного больше времени, но красивая улыбка того стоит."
+      title: "Молочные зубы важны",
+      subtitle: "Передние зубы меняются в 6 лет, но задние молочные зубы сохраняются до 10-13 лет.",
+      part1:"Правильный уход за молочными зубами должен начинаться, как только они начинают появляться в рту. Сначала следует использовать влажную салфетку для 'полировки' зубов, но с появлением большего числа зубов следует аккуратно использовать детскую зубную щетку хотя бы дважды в день. Фтор в правильной дозировке делает эмаль более прочной. Хотя во многих общинах фтор добавляется в воду, его также наносят на зубы после каждой профессиональной чистки.",
+      description: "Молочные зубы важны для жевания, сохранения места для постоянных зубов и нормального развития челюсти.",
+      thumbSucking: "Сосание пальца",
+      nursingBottleCavities: "Кариес от бутылочки",
+      eruptionOfTeeth: "Прорезывание зубов",
+      dentalEmergencies: "Стоматологическая неотложная помощь",
+      grinding: "Скрежетание зубами (Бруксизм)",
+      orthodontics: "Ортодонтия"
     }
-  };
 
 
-
+  }
   return (
     <Container fluid className="py-5" style={{ backgroundColor: '#3c1a6f', color: '#fff' }}>
       <Row className="justify-content-center align-items-center text-center">
         <Col md={6}>
-          <h1 className="display-4">{translations[lang].care}</h1>
-          <p>{translations[lang].misalignedTeeth}</p>
+          <h1 className="display-4">{translations[lang].title}</h1>
+          <p>{translations[lang].subtitle}</p>
         </Col>
         <Col md={4}>
           <Card className="border-0">
@@ -82,17 +64,17 @@ function ChildrensTeeth() {
       </Row>
 
       <Row className="py-5 bg-light text-dark">
-        <h1 style={{ fontWeight: "600", fontSize: "50px" }}>{translations[lang].createSmile}</h1>
-        <p style={{ color: "#6F3D85", fontSize: "23px", fontWeight: "600" }}>
-          {translations[lang].orthodonticsEffect}
+        <h1 style={{ fontWeight: "600", fontSize: "25px" }}>{translations[lang].description}</h1>
+        <p style={{ color: "#6F3D85", fontSize: "20px", fontWeight: "600" }}>
+          {translations[lang].par1}
         </p>
         <br />
         <br />
-        <p style={{ color: "#54595F", fontSize: "20px" }}>
-          {translations[lang].alignmentDescription}
-        </p>
+        
+        <br />
+
         <ul style={{ listStyleType: "none" }}>
-          <li>&#x25B8; <b>{translations[lang].toothBrushing}</b> - {translations[lang].toothBrushingDesc}</li>
+          <li>&#x25B8; <b>{translations[lang].thumbSucking}</b> - {translations[lang].toothBrushingDesc}</li>
           <li>&#x25B8; <b>{translations[lang].interdentalBrush}</b> - {translations[lang].interdentalBrushDesc}</li>
           <li>&#x25B8; <b>{translations[lang].flossThreader}</b> - {translations[lang].flossThreaderDesc}</li>
           <li>&#x25B8; <b>{translations[lang].oralIrrigation}</b> - {translations[lang].oralIrrigationDesc}</li>
