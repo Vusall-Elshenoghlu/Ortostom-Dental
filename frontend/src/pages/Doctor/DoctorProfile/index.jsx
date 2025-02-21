989be0fed978b381c2ceb9ff10bc2324c520ee6f
Vusall-Elshenoghlu <vusaleh-af206@code.edu.az>
2025-02-21 22:16:17 +0400

    21-02-2025 22:26

--- frontend/src/pages/Doctor/DoctorProfile/index.jsx
+++ frontend/src/pages/Doctor/DoctorProfile/index.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import {toast} from "react-toastify"
+import axios from "axios"
 function DoctorProfile() {
+  const [profileData, setProfileData] = useState(false)
+  const [dToken,setDToken] = useState(localStorage.getItem("doctorToken" ? localStorage.getItem("doctorToken") : ""))
+  
+  
+  const getProfileData = async () => {
+    try {
+
+      const { data } = axios.get("http://localhost:3000/doctors/profile", {
+        headers: {
+          Authorization: `Bearer ${dToken}`,
+        },
+      })
+      console.log(data)
+      
+      if(data.success){
+        setProfileData(data.profileData)
+        console.log(data.profileData)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() =>{
+      getProfileData()
+    
+  },[])
   return (
-    <div style={{marginTop:"100px"}}>
+    <div style={{ marginTop: "100px" }}>
       Doctor Profile
     </div>
   )
